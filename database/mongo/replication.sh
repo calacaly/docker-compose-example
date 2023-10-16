75773902820a565d5b19c19c02a62c95ea680ecc
root <root@kali.kail>
2023-10-16 21:43:34 +0800

    new

--- database/mongo/replication.sh
+++ database/mongo/replication.sh
@@ -5,15 +5,14 @@ MONGO_USER="root"
 MONGO_PASS="123456"
 MONGO_PORT="27017"
 
-
 MONGO_DSN="mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_PRIMARIY_HOST}:${MONGO_PORT}"
 
-RS_CONF='rs.initiate({_id:"rs0",members:[{_id:0,host:"mongo0:27017" },{_id:1,host:"mongo1:27017"},{_id:2,host:"mongo2:27017"}]})'
+RS_CONF='rs.initiate({_id:"rs0",members:[{_id:0,host:"mongo0:27017",priority:1},{_id:1,host:"mongo1:27017",priority:0.5},{_id:2,host:"mongo2:27017",priority:0.5}]})'
+
+CREATE_USER_CONF='db.createUser({user:'"'${MONGO_USER}'"',pwd:'"'${MONGO_PASS}'"',roles:[{role:"root",db:"admin"}]})'
 
 
 function Config(){
-	sed -i "s@MONGO_INITDB_ROOT_USERNAME:.*@MONGO_INITDB_ROOT_USERNAME: ${MONGO_USER}@g" ${MONGO_COMPOSE_FILE}
-	sed -i "s@MONGO_INITDB_ROOT_PASSWORD:.*@MONGO_INITDB_ROOT_PASSWORD: ${MONGO_PASS}@g" ${MONGO_COMPOSE_FILE}
 	rm -rf docker-compose.yml;cp ${MONGO_COMPOSE_FILE} docker-compose.yml
 }
 
@@ -32,12 +31,15 @@ function Start(){
 function Init(){
 	# init replication
 	${DOCKER_COMPOSE} --file ${MONGO_COMPOSE_FILE} exec ${MONGO_PRIMARIY_HOST} mongosh --eval "${RS_CONF}"
+	sleep 10s
+	# init password
+	${DOCKER_COMPOSE} --file ${MONGO_COMPOSE_FILE} exec ${MONGO_PRIMARIY_HOST} mongosh admin --eval "${CREATE_USER_CONF}"
 }
 
 
 function Status(){
 	# status replication
-	${DOCKER_COMPOSE} --file ${MONGO_COMPOSE_FILE} exec ${MONGO_PRIMARIY_HOST} mongosh --eval 'rs.status("rs0")'
+	${DOCKER_COMPOSE} --file ${MONGO_COMPOSE_FILE} exec ${MONGO_PRIMARIY_HOST} mongosh ${MONGO_DSN} --eval 'rs.status("rs0")'
 }
 Config
 GenKey
